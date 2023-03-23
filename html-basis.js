b2496f9240edb3e618c887d1822e7a029a136c84
sunyj <923304784@qq.com>
2023-03-23 20:53:35 +0800

    css

---
+++ html-basis.js
@@ -0,0 +1,13 @@
+// var textAnimate = eval(jscex.complie);
+// var together = new Date();
+// together.setFullYear;
+// together.setHours;
+// together.setMinutes;
+// together.setSeconds;
+// together.setMilliseconds;
+// SharedWorker.typewrite();
+// fadein;
+// while (true) {
+//   timeelapse(together);
+//   awaitjscex.async.sleep();
+// }
